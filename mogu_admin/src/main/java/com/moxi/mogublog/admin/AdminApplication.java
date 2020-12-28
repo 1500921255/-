e7952b4bce4a823769e0396bd38a5428d22d9558
1500921255 <1500921255@qq.com>
2020-12-28 18:40:00 +0800

    土豆博客

---
+++ mogu_admin/src/main/java/com/moxi/mogublog/admin/AdminApplication.java
@@ -0,0 +1,65 @@
+package com.moxi.mogublog.admin;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableTransactionManagement
+@SpringBootApplication
+@EnableOpenApi
+@EnableDiscoveryClient
+@EnableCaching
+@EnableRabbit
+@EnableFeignClients("com.moxi.mogublog.commons.feign")
+@ComponentScan(basePackages = {
+        "com.moxi.mogublog.commons.config",
+        "com.moxi.mogublog.commons.fallback",
+        "com.moxi.mogublog.utils",
+        "com.moxi.mogublog.admin",
+        "com.moxi.mogublog.xo.utils",
+        "com.moxi.mogublog.xo.service"
+})
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+    /**
+     * 设置时区
+     */
+    @PostConstruct
+    void setDefaultTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //配置允许跨域访问的路径
+                registry.addMapping("/**/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("")
+                        .maxAge(3600);
+            }
+        };
+    }
+}
